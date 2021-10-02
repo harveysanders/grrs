94e8f3ab7003f5666ad36a00d49b7d4e9719ddd1
Harvey Sanders <sanders.harvey@gmail.com>
2021-10-02 14:41:55 -0400

    Using BufReader

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 use structopt::StructOpt;
 
 /// Search for a pattern in a file and display the lines that contain it.
@@ -10,11 +13,23 @@ struct Cli {
 
 fn main() {
     let args = Cli::from_args();
-    let content = std::fs::read_to_string(&args.path).expect("Could not read file");
-
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+    if let Ok(lines) = read_lines(&args.path) {
+        for line in lines {
+            if let Ok(ip) = line {
+                if ip.contains(&args.pattern) {
+                    println!("{}", ip);
+                }
+            }
         }
     }
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
